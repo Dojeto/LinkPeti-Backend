45fbb676ffb00c602b1a10db3baf86d844e79195
Dojeto <omlimdiwala25@gmail.com>
2024-04-12 08:37:32 +0530

    Things Works Differently in ServerLess Architecture

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ import cors from 'cors'
 import { config } from 'dotenv';
 import auth from './routes/jwtauth.js'
 import dashboard from './routes/dashboard.js'
-import mongo from './models/db.js'
+// import mongo from './models/db.js'
+import mongoose from 'mongoose';
 import managelinks from './routes/managelinks.js'
 import home from './routes/home.js'
 import corsOptions from "./config/corsOptions.js";
 
 const port = process.env.PORT || 3000;
+const uri = process.env.MONGO_URL;
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URL);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
 
 const app = express();
 config();
@@ -22,6 +34,8 @@ app.use('/auth',auth);
 app.use('/dashboard',dashboard);
 app.use('/manage',managelinks);
 
-app.listen(port,()=>{
-    console.log(`listening on ${port}`);
+connectDB().then(()=>{
+    app.listen(port,()=>{
+        console.log(`listening on ${port}`);
+    })
 })
